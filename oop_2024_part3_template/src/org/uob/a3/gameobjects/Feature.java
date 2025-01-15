5afaf49c8b286d5d70c6a98f1ae1b1993cbb76f8
Ben Iskauskas <bsi418@student.bham.ac.uk>
2025-01-15 12:21:53 +0000

    features EASY!

--- oop_2024_part3_template/src/org/uob/a3/gameobjects/Feature.java
+++ oop_2024_part3_template/src/org/uob/a3/gameobjects/Feature.java
@@ -1,14 +1,13 @@
 package org.uob.a3.gameobjects;
 
-/**
- * Represents a feature in the game, which is a type of game object that can be part of a room or environment.
- *
- * <p>
- * Features are typically static objects with a name, description, and visibility state. They may
- * also serve as interactable elements in the game.
- * </p>
- */
 public class Feature extends GameObject {
 
-  
+    //using super class features, nothing to be added
+    public Feature(String id, String name, String description) {
+        super(id, name, description);
+    }
+
+    @Override
+    public String toString() {return super.toString();
+    }
 }
