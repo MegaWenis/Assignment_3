96a98913dbd9982045c975ae80919e74c6da25e4
Ben Iskauskas <bsi418@student.bham.ac.uk>
2025-01-15 12:07:05 +0000

    player and notebook

--- oop_2024_part3_template/src/org/uob/a3/gameobjects/Notebook.java
+++ oop_2024_part3_template/src/org/uob/a3/gameobjects/Notebook.java
@@ -1,18 +1,59 @@
 package org.uob.a3.gameobjects;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Represents a notebook in the game, which is a type of {@code GameObject}.
- *
- * <p>
- * The notebook is used to store collected clues and manage notes. Players can use the notebook
- * to keep track of their progress in solving the game's mystery.
- * </p>
- */
 public class Notebook extends GameObject {
 
-   
+    private List<Clue> clues;
+
+    //contructor
+    public Notebook() {
+        this.clues = new ArrayList<>();
+    }
+
+    //adds a new clue unless you aready have it
+    public void addClue(Clue clue) {
+        if (!clues.contains(clue)) {
+            clues.add(clue);
+        }
+    }
+
+    //returns the list of clues
+    public List<Clue> getClues() {
+        return new ArrayList<>(clues);
+    }
+
+    //checks if you contain all the required clues
+    public boolean containsAll(List<String> requiredClues) {
+        for(String requiredClue : requiredClues) {
+            boolean found =false;
+            for (Clue clue : clues) {
+                if (clue.getName().equals(requiredClue)) {
+                    found = true;
+                    break;}
+            }
+            if (!found) {
+                return false;
+            }
+        }return true;
+    }
+
+    public int getNumberOfClues() {
+        return clues.size();
+    }
+
+    // Returns a string representation of the notebook by calling the superclass's toString method
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder("You have the following clues:\n");
+
+        for (Clue clue:clues){
+
+            output.append("- ").append(clue.getName()).append(": ").append(clue.getDescription()).append("\n");
+
+        }
+        return output.toString();
+    }
 }
+
--- oop_2024_part3_template/src/org/uob/a3/gameobjects/Player.java
+++ oop_2024_part3_template/src/org/uob/a3/gameobjects/Player.java
@@ -9,6 +9,33 @@ import java.util.ArrayList;
  * The player can collect clues, manage their notebook, and interact with the game world.
  * </p>
  */
-public class Player {
-   
+
+public class Player extends Object {
+
+    private String name;
+    private Notebook notebook;
+
+    //constructor for Player
+    public Player() {
+        this.name = "Unknown";
+        this.notebook = new Notebook();
+    }
+
+    //contructs player given their name
+    public Player(String name) {
+        this.name = name;
+        this.notebook = new Notebook();
+    }
+
+    //getters
+    public String getName(){return this.name;
+    }
+    public Notebook getNotebook() {return this.notebook;}
+
+    // Returns a string representation of the player's current state, including their name and the contents of their notebook
+    @Override
+    public String toString() {
+        return "Name:" + name + "\nNotebook: " + notebook.getNumberOfClues() + " clues collected.";
+    }
 }
+
