629bf9c50bb8cec46e7ef6442e0deb84965fd1ae
Ben Iskauskas <bsi418@student.bham.ac.uk>
2025-01-18 15:12:16 +0000

    s

--- oop_2024_part3_template/src/org/uob/a3/commands/Combine.java
+++ oop_2024_part3_template/src/org/uob/a3/commands/Combine.java
@@ -9,45 +9,42 @@ public class Combine extends Command {
     }
 
     public String execute(GameState gameState) {
+        String itemsComma = value.trim();
+        String extractedItems = itemsComma.substring(8).trim(); // Remove 'combine ' and trim
+        System.out.println("Extracted items: " + extractedItems);
 
-        String itemsComma = value;
-        String[] items = itemsComma.substring(8).split(",");
-
+        String[] items = extractedItems.split(",");
         if (items.length == 2) {
             Notebook notebook = gameState.getPlayer().getNotebook();
             String item1 = items[0].trim();
             String item2 = items[1].trim();
 
+            System.out.println("Parsed item1: " + item1);
+            System.out.println("Parsed item2: " + item2);
 
             Clue clue1 = notebook.getClueByName(item1);
             Clue clue2 = notebook.getClueByName(item2);
 
-            if (clue1 ==null || clue2 ==null) {
+            if (clue1 == null || clue2 == null) {
                 return "One or both clues were not found in your notebook.";
             }
 
-            System.out.println(item1 + " " +item2);
-            System.out.println(clue2.getHint() + " " +clue1.getHint());
-            System.out.println(clue2.getName() + " " + clue1.getName());
-
-
             if (notebook.hasClue(item1) && notebook.hasClue(item2)) {
-
                 if (clue1.getHint().equalsIgnoreCase(clue2.getHint())) {
-                    Clue newClue= gameState.getMansion().getLocationByName("store").getClue(clue1.getHint());
+                    Clue newClue = gameState.getMansion().getLocationByName("store").getClue(clue1.getHint());
                     notebook.addClue(newClue);
-                    return "You combined the " + item1+ " with " + item2 + " into "+ newClue.getName();
+                    return "You combined the " + item1 + " with " + item2 + " into " + newClue.getName();
                 } else {
                     return "These items do not combine.";
                 }
             } else {
                 return "You don't have these clues.";
             }
-
         } else {
             return "Invalid input. Please provide two items separated by a comma.";
         }
     }
+
 
 
 }
